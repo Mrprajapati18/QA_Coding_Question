55cfbd5178a5e556f7cee348427887df0afd4911
MrPrajapati18 <durgeshkumar778080@gmail,com>
2025-02-06 17:23:05 +0530

    Count Character String

--- QA Coding Question/src/Com/QA_Coding_Practice/CountCharacter.java
+++ QA Coding Question/src/Com/QA_Coding_Practice/CountCharacter.java
@@ -1,9 +1,22 @@
 package Com.QA_Coding_Practice;
 
+import java.util.Scanner;
+
 //  Write a Java Program to count the number of characters in a given String?
+
 public class CountCharacter {
 	public static void main(String[] args) {
+		Scanner Scan = new Scanner(System.in);
 		
+		System.out.println("Enter the number of String");
+		
+		String input = Scan.nextLine();
+		
+		int count = input.length();
+		
+		System.out.println("Character of the number is " + count);
+		
+		Scan.close();
 	}
 
 }
